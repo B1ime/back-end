c0e213fc59ea856bf35e81678db9ae033e1b298c
Byeong Gyu Choi <bisschoi9541@gmail.com>
2018-10-21 23:31:29 +0900

    데이터베이스 헬퍼 함수 클래스화

--- src/database/users.ts
+++ src/database/users.ts
@@ -5,19 +5,18 @@ export interface B1imeUser {
   name: string;
 }
 
-interface UserORM {
-  create: (user: B1imeUser) => Promise<DynamoDB.Types.PutItemOutput>;
-}
+export class User {
+  private readonly endpoint: string = process.env.DATABASE_HOST as string;
+  private readonly db: DynamoDB = new DynamoDB({
+    endpoint: this.endpoint
+  });
 
-const dynamoDB = new DynamoDB({ endpoint: "http://localhost:8000" });
-
-export const B1imeUser: UserORM = {
   /**
    * 유저를 생성합니다. 이미 존재하는 유저의 경우 업데이트가 됩니다.
    * @param user: 유저의 id, 이름 정보
    * @return 생성된 유저 정보를 포함한 Promise
    */
-  create: async user => {
+  public async create(user: B1imeUser) {
     const params: DynamoDB.Types.PutItemInput = {
       TableName: "B1imeTable",
       Item: {
@@ -26,6 +25,6 @@ export const B1imeUser: UserORM = {
       },
       ReturnValues: "ALL_OLD"
     };
-    return await dynamoDB.putItem(params).promise();
-  },
-};
+    return await this.db.putItem(params).promise();
+  }
+}
